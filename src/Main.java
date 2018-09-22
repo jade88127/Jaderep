77c8f694d871df3f11469402501b10c5a274a733
jade88127 <dawn88127@gmail.com>
2018-09-22 22:52:36 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args)
+    {
+        String str1;
+        String str2;
+        Integer x;
+
+        str1 = "亞州大學";
+        str2 = "張三";
+        x    = 5;
+	    System.out.printf("%s就讀的校名是:%s\n年齡:%s", str2, str1, x);
+    }
+}
